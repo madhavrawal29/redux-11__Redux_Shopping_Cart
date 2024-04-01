9aac37d002efabbbdfc4465933f8a8fcdc2d8309
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-04-01 13:03:09 +0530

    Using an action creator Thunk

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { uiActions } from "./components/store/ui-slice";
+import { sendCartData } from "./components/store/cart-slice";
 
 let isInitial = true;
 
@@ -15,46 +15,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sneding...",
-          message: "Sending Cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://shoppingcartusingredux-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent Cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending Cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
